8d30dc4df786b862435d738c37b700719a49e16c
austinChappell <Austin.W.Chappell@gmail.com>
2019-06-27 23:21:53 -0500

    change text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          The text changed again
+          The text changed from the feature branch
         </p>
         <a
           className="App-link"
